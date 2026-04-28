625e90b32d2c4aa505324c9bae286228d2a74ad6
Germán Pancardo <germanpancardo88@aragon.unam.mx>
2026-04-28 02:17:18 -0600

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+// after
+import netlify from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+// svelte.config.js
+export default {
+  kit: {
+    adapter: netlify(),
+    prerender: {
+      entries: ['/', '/about'] // list pages you actually want prerendered
+    }
+  }
 };
-
-export default config;
